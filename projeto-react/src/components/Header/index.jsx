b4560c5c7dbfcd03df2f05d1d9a837a884b14e91
Martin Schwans <amrastasu@gmail.com>
2019-06-04 18:08:54 -0300

    Adicionado material ui

--- projeto-react/src/components/Header/index.jsx
+++ projeto-react/src/components/Header/index.jsx
@@ -1,20 +1,22 @@
-import React from 'react';
+import React from "react";
 
-import logo from '../../assets/logo.png';
+import logo from "../../assets/logo.png";
 
-import './style.css';
+import "./style.css";
+
+import Example from "../../components/Demo";
 
 const Header = () => (
-    <div>
-        <header className="header">
-            <div className="mdl-grid">
-                <div className="mdl-cell--12-col">
-                    <img alt="Logo" src={logo} className="header__logo" />
-                </div>
-            </div>
-
-        </header>
-    </div>
+  <div>
+    <header className="header">
+      <div className="mdl-grid">
+        <div className="mdl-cell--12-col">
+          <img alt="Logo" src={logo} className="header__logo" />
+        </div>
+        <div />
+      </div>
+    </header>
+  </div>
 );
 
-export default Header   
+export default Header;
--- projeto-react/src/pages/Search/index.jsx
+++ projeto-react/src/pages/Search/index.jsx
@@ -1,53 +1,53 @@
-import React, { Component, Fragment } from 'react';
-import { Link } from 'react-router-dom';
+import React, { Component, Fragment } from "react";
+import { Link } from "react-router-dom";
 
-import axios from 'axios';
+import axios from "axios";
 
-import Button from '../../components/Button'
+import Button from "../../components/Button";
+import Example from "../../components/Demo"
 
 class Search extends Component {
+  constructor() {
+    super();
 
-    constructor() {
-        super();
+    this.onSearch = this.onSearch.bind(this);
 
-        this.onSearch = this.onSearch.bind(this);
+    this.state = {
+      results: []
+    };
+  }
 
-        this.state = {
-            results: [],
-        }
-    }
+  onSearch(event) {
+    const value = event.currentTarget.value;
+    axios
+      .get(`https://api.mercadolibre.com/sites/MLU/search?q=${value}`)
+      .then(({ data }) => {
+        this.setState({
+          results: data.results
+        });
+      });
+  }
+  renderItem(item) {
+    return (
+      <li key={item.id}>
+        <span>{item.id}</span>
+        <span>{item.title}</span>
+        <Link to={`/product/${item.id}`}> Abrir produto</Link>
+      </li>
+    );
+  }
 
-    onSearch(event) {
-        const value = event.currentTarget.value;
-        axios.get(`https://api.mercadolibre.com/sites/MLU/search?q=${value}`)
-            .then(({ data }) => {
-                this.setState({
-                    results: data.results,
-
-                })
-            })
-
-    }
-    renderItem(item) {
-        return (
-            <li key={item.id}>
-                <span>{item.id}</span>
-                <span>{item.title}</span>
-                <Link to={`/product/${item.id}`}> Abrir produto</Link>
-            </li>
-        )
-    }
-
-    render() {
-        return (
-            <Fragment>
-                <div><input type="text" onChange={this.onSearch} /></div>
-                <ul>
-                    {this.state.results.map(this.renderItem)}
-                </ul>
-            </Fragment>
-        );
-    }
+  render() {
+    return (
+      <Fragment>
+          <Example></Example>
+        <div>
+          <input type="text" onChange={this.onSearch} />
+        </div>
+        <ul>{this.state.results.map(this.renderItem)}</ul>
+      </Fragment>
+    );
+  }
 }
 
 export default Search;
