bddafc5d02696edb4ce766a536c07d97f298f08b
Martin Schwans <amrastasu@gmail.com>
2019-06-05 19:28:59 -0300

    Adicionado o formato em reais do preço do produto

--- projeto-react/src/pages/Products/index.jsx
+++ projeto-react/src/pages/Products/index.jsx
@@ -1,75 +1,97 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component, Fragment } from "react";
 
-import Button from '../../components/Button'
+import Button from "../../components/Button";
 
-import axios from 'axios';
-
-import './style.css';
-
+import axios from "axios";
+import Grid from "@material-ui/core/Grid";
+import "./style.css";
+import Numeral from "numeral";
+import "numeral/locales/pt-br";
+Numeral.locale("pt-br");
 
 class Products extends Component {
+  constructor(props) {
+    super(props);
 
-    constructor(props) {
-        super(props);
+    this.state = {
+      id: props.match.params.id,
+      loading: true,
+      data: {}
+    };
+    console.log(props.match.params.id);
+  }
 
-        this.state = {
-            id: props.match.params.id,
-            loading: true,
-            data: {},
-        }
-        console.log(props.match.params.id);
-    }
-
-    componentDidMount() {
-        axios.all([
-            axios.get(`https://api.mercadolibre.com/items/${this.state.id}`),
-            axios.get(`https://api.mercadolibre.com/items/${this.state.id}/description`)
-        ]).then(([item, description]) => {
-            console.log(item)
-            console.log(description)
-            this.setState({
-                data: {
-                    ...item.data,
-                    description: description.data.plain_text,
-                },
-                loading: false,
-            });
-        }).catch((err) => {
-            console.log(err)
-        })
-    }
-
-    renderContent() {
-        const { data } = this.state;
-        console.log(data)
-
-        return (
-            <div>
-                <Fragment >
-
-                    <div className="mdl-grid mdl-shadow--6dp card">
-
-                        <h1><img src={data.pictures[0].url} /></h1>
-                        <div className="mdl-cell mdl-cell--6-col">
-                            <h4>{data.title}</h4>
-                            <p>Preço: {data.base_price}</p>
-                            <p><Button label="Comprar" /></p>
-                            <p>{data.description}</p>
-                        </div>
-
-                    </div>
-                </Fragment>
-            </div >
+  componentDidMount() {
+    axios
+      .all([
+        axios.get(`https://api.mercadolibre.com/items/${this.state.id}`),
+        axios.get(
+          `https://api.mercadolibre.com/items/${this.state.id}/description`
         )
-    }
+      ])
+      .then(([item, description]) => {
+        console.log(item);
+        console.log(description);
+        this.setState({
+          data: {
+            ...item.data,
+            description: description.data.plain_text
+          },
+          loading: false
+        });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
 
-    render() {
-        const { loading } = this.state;
+  renderContent() {
+    const { data } = this.state;
+    console.log(data);
 
-        return loading ?
-            <div id="p2" className="mdl-progress mdl-js-progress mdl-progress__indeterminate"></div>
-            : this.renderContent();
-    }
+    return (
+      <div>
+        <Fragment>
+          <Grid
+            className="mdl-grid mdl-shadow--6dp card"
+            container
+            direction="row"
+            justify="flex-start"
+            alignItems="flex-start"
+          >
+            <Grid>
+              <h1>
+                <img src={data.pictures[0].url} />
+              </h1>
+            </Grid>
+            <Grid className="mdl-cell mdl-cell--6-col">
+              <h4>{data.title}</h4>
+              <p>Preço: {Numeral(data.base_price).format("$0.00")}</p>
+              <p>
+                <Button label="Comprar" />
+              </p>
+            </Grid>
+            <Grid item xs={6} al>
+              <p>{data.description}</p>
+            </Grid>
+          </Grid>
+        </Fragment>
+      </div>
+    );
+  }
+
+  render() {
+    const { loading } = this.state;
+
+    return loading ? (
+      <div
+        id="p2"
+        className="mdl-progress mdl-js-progress mdl-progress__indeterminate"
+      />
+    ) : (
+      this.renderContent()
+    );
+  }
 }
 
-export default Products
+export default Products;
