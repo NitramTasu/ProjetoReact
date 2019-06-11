7ff9f66b3b7cc320e262f4f058e25779650adc42
Martin Sch <amrastasu@gmail.com>
2019-06-11 01:14:08 -0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ProjetoReact/precache-manifest.c77e0d793b93dc26ce939cc0fb247d34.js"
+  "/ProjetoReact/precache-manifest.763ffeaca49c8a2469eaa197091c5de8.js"
 );
 
 self.addEventListener('message', (event) => {
